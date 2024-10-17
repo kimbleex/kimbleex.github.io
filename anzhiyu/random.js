d00195a29213196d54f3846a583e3507e03231dc
kimbleex <kimbleex@outlook.com>
2024-10-17 12:13:10 +0800

    Site updated: 2024-10-17 12:13:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,56 @@
 var posts=["2020/12/09/3.Recommend01_tqdm/","2020/03/11/1.Hexo_Image_Process/","2021/10/25/2.Github_Commands/","2021/03/18/4.Selenium_Read_UsersInfo/","2024/07/25/5.Python Asyncio异步操作/","2024/08/05/8.OpenAI接口实现图片分析/","2024/08/02/6.Python调用Openai接口实现chatGPT问答/","2022/08/15/7.AmazonSpider/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
+  };var friend_link_list=[{"name":"Hexo","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"Kimbleex","link":"https://xujunliang666.com/","avatar":"https://picgo.kimbleex.top/THEME/anzhiyu/MainWebPage/Avatar.jpg","descr":"丢掉幻想，准备斗争","siteshot":"https://picgo.kimbleex.top/THEME/anzhiyu/Link/Rec/Kimbleex/BC.png","color":"vip","tag":"技术"},{"name":"Tristan","link":"https://blog.csdn.net/qq_51065387/article/details/128984426","avatar":"https://picgo.kimbleex.top/THEME/anzhiyu/Link/Friends/Tristan.png","descr":"金融领域马尔可夫模型实际运用","siteshot":"https://picgo.kimbleex.top/THEME/anzhiyu/Link/Rec/Tristan/BG.png","color":"vip","tag":"技术"},{"name":"Kimbleex","link":"https://xujunliang666.com/","avatar":"https://picgo.kimbleex.top/THEME/anzhiyu/MainWebPage/Avatar.jpg","descr":"贪生怕死勿入斯门，升官发财请往他处","recommend":true},{"name":"Tristan","link":"https://blog.csdn.net/qq_51065387?type=blog","avatar":"https://picgo.kimbleex.top/THEME/anzhiyu/Link/Friends/Tristan.png","descr":"大骏骏我的宝贝😍❤","recommend":true}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
